5c8a92aae6837ae060c495004d9c22cda635081e
brombergyuval <brombergyuval@gmail.com>
2026-06-29 13:52:13 +0300

    Refactor script.js for game setup
    Removed expo and SQRT functions, added grid and game variables.

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-function expo (){
-    let inputExpo = prompt("Please enter a number");
-    if ((inputExpo*inputExpo)%2==0){
-        document.writeln("<div id=expo>exponent is even</div>");
+let grid = document.getElementById('grid');
+let scoreDisplay = document.getElementById('score');
+let squares =[];
+let currentSnake = [2,1,0];
+let direction = 1;
+let appleIndex = 0;
+let score = 0;
+let timerId = 0;
+let intervalTime = 200;
+function createBoard(){    
+    for(let i = 0;i<400;i==){
+        let square = document.createElement('div');
+        let squareChild[];
+        grid.appendChild(square);
+        squares.push(square);
     }
 }
-
-function SQRT(){
-    let inputSQRT = prompt("Please enter a number");
-    document.writeln("<div class=test>"+Math.sqrt(inputSQRT)+"</div>");
-}
-
-expo()
-
-SQRT()
+createBoard();
